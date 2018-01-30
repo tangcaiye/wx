1854451b3d4c950379202d808746fc78ed815ea1
唐向荣 <641418330@qq.com>
2018-01-30 16:22:25 +0800

    swiper

--- 1705-wx2/pages/index/index.js
+++ 1705-wx2/pages/index/index.js
@@ -1,6 +1,19 @@
 Page({
   data: {
-
+    products: [    'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+    ]
+  },
+  onLoad () {
+    // wx.request({
+    //   url: "http://localhost:3000/products",
+    //   success: res => {
+    //     this.setData({
+    //       products: res.data
+    //     })
+    //   }
+    // })
   },
   tolowerHandle () {
     console.log('快到底le ')
