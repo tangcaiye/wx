c9f38512dfbd38db294c1ba076849b43984bb194
唐向荣 <641418330@qq.com>
2018-01-30 14:26:35 +0800

    小程序的自定义组件

---
+++ 1705-wx2/components/component/component.js
@@ -0,0 +1,26 @@
+// components/component/component.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    innerText: {
+      type: String,
+      value: 'default value'
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+  
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
